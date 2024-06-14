8904896fcbe01a66f44495e93bf7c43ab38b9997
FlyingSnake <fasttutle131@gmail.com>
2024-06-14 23:55:58 +0900

    add helm: cert-manager

--- addon_certmanager.tf
+++ addon_certmanager.tf
@@ -2,7 +2,7 @@ resource "helm_release" "cert_manager" {
   count            = var.helm_addons["cert-manager"] ? 1 : 0
   name             = "cert-manager"
   repository       = "https://charts.jetstack.io"
-  chart            = "aws-load-balancer-controller"
+  chart            = "cert-manager"
   namespace        = "cert-manager"
   create_namespace = true
 }
